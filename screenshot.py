92892340a8cd451c3b7c682f5be4e7e45c960d52
Archer_小A <qddegtya@gmail.com>
2016-01-04 13:51:07 +0800

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -15,12 +15,12 @@ from time import sleep
 class AndroidScreenStream(object):
 
     def __init__(self):
-        self.devices=[]
-        self.currentdevice=None
-        self.rawimgdata=None
-        self.rawscreen=None
+        self.devices = []
+        self.currentdevice = None
+        self.rawimgdata = None
+        self.rawscreen = None
 
-    def wait_device_lists(self,bridge):
+    def wait_device_lists(self, bridge):
         """获取设备列表信息"""
         count = 0
         while(bridge.hasInitialDeviceList() == False):
@@ -41,7 +41,7 @@ class AndroidScreenStream(object):
         self.devices.append(mybridge.getDevices())
         return self
 
-    def get_device_by_index(self,index):
+    def get_device_by_index(self, index):
         """根据index获取单个当前设备"""
         AndroidDebugBridge.init(False)
         mybridge = AndroidDebugBridge.createBridge()
@@ -63,28 +63,28 @@ class AndroidScreenStream(object):
         self.rawscreen = self.currentdevice.getScreenshot()
         return self
 
-    def save_screen_img(self,path = os.path.join(os.getcwd(),'current_frame.jpeg'),islandscape = False):
+    def save_screen_img(self, path = os.path.join(os.getcwd(), 'current_frame.jpeg'), islandscape = False):
         """将当前屏幕图像数据保存为图片"""
         if self.rawscreen is not None:
             landscape = islandscape
             # 横屏尺寸处理
             imgwidth = self.rawscreen.height if landscape else self.rawscreen.width
             imgheight = self.rawscreen.width if landscape else self.rawscreen.height
-            image = BufferedImage(imgwidth,imgheight,BufferedImage.TYPE_INT_RGB)
+            image = BufferedImage(imgwidth, imgheight, BufferedImage.TYPE_INT_RGB)
             if image.getHeight() != imgheight or image.getWidth() != imgwidth:
-                image = BufferedImage(imgwidth,imgheight,BufferedImage.TYPE_INT_RGB)
+                image = BufferedImage(imgwidth, imgheight, BufferedImage.TYPE_INT_RGB)
             index = 0
             indexInc = self.rawscreen.bpp >> 3
             for y in range(self.rawscreen.height):
                 for x in range(self.rawscreen.width):
                     value = self.rawscreen.getARGB(index)
                     if landscape:
-                        image.setRGB(y,self.rawscreen.width - x - 1,value)
+                        image.setRGB(y, self.rawscreen.width - x - 1, value)
                     else:
-                        image.setRGB(x,y,value)
+                        image.setRGB(x, y, value)
                     index += indexInc
             try:
-                ImageIO.write(image,"JPEG",File(path))
+                ImageIO.write(image, "JPEG", File(path))
             except IOError:
                 print "发生异常!"
                 sys.exit()
@@ -97,4 +97,3 @@ if __name__ == "__main__":
     ShotDevice = ASS.get_device_by_index(0)
     while True:
         ShotDevice.get_current_rawscreen().save_screen_img()
-        
