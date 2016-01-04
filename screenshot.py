b30a548d1d473ebd5b31e276978ad40608d95a36
Archer_小A <qddegtya@gmail.com>
2016-01-04 13:53:16 +0800

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-import sys,os
+import sys, os
 sys.path.append(r'C:\\ddmlib.jar')
 sys.path.append(r'C:\\guava17.jar')
 sys.path.append(r'C:\\rt.jar')
